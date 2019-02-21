e6ee5fe1ccc0e548a4fb23564483dd55e67b7861
Pau Cervera i Badia <pau.cervera@capside.com>
2019-02-21 12:34:24 +0100

    Add the meaning of life

--- server.py
+++ server.py
@@ -9,6 +9,13 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
     def do_GET(self):
         print("Requested path: {}".format(self.path))
+        if self.path == '/life':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            response = bytearray('The meaning of life is 42', 'utf-8')
+            self.wfile.write(response)
+            return
         super().do_GET()
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
