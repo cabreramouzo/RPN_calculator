0c56adb5bca26be1ffddc856cafc68f066e0383d
Pau Cervera i Badia <pau.cervera@capside.com>
2019-02-21 11:30:53 +0100

    Subclass the default handler and use it right away

--- server.py
+++ server.py
@@ -3,7 +3,10 @@ import socketserver
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
+#Handler = http.server.SimpleHTTPRequestHandler
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    pass
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
